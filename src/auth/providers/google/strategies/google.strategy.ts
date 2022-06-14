2ac4e9bc08d19720c96a71784ea840a85c4aeb22
JackDrakkar <jack.d.drakkar@gmail.com>
2022-06-14 17:48:56 +0300

    add basic redirection for google auth

--- src/auth/providers/google/strategies/google.strategy.ts
+++ src/auth/providers/google/strategies/google.strategy.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
-import { Strategy, VerifyCallback, Profile } from 'passport-google-oauth20';
+import { Request } from 'express';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { APP_NAMESPACE, IAppConfig } from '../../../../configuration/app/app.config';
 import { AUTH_PROVIDERS_NAMESPACE, IAuthProvidersConfig } from '../../../../configuration/providers/providers.config';
 
@@ -16,15 +17,27 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: authProvidersCfg.google.clientSecret,
       callbackURL: `${appCfg.baseUrl}/auth/google/redirect`,
       scope: ['email', 'profile'],
+      passReqToCallback: true,
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback): Promise<any> {
-    const user = {
+  async validate(
+    req: Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const user: any = {
       id: profile.id,
       provider: profile.provider,
       username: profile.emails[0]?.value,
     };
+
+    if (req.query.state) {
+      const state = Buffer.from(req.query.state as string, 'base64url').toString('utf8');
+      user.state = JSON.parse(state);
+    }
 
     done(null, user);
   }
